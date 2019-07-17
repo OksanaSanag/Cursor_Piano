f22ddec4f1d315ecaf85cda9aeb58ca27b64ff58
OksanaSanag <oksanasanag@gmail.com>
2019-07-17 12:24:37 +0300

    final version

--- piano.js
+++ piano.js
@@ -1,32 +1,31 @@
+const notes = [
+    {src: './sounds/1.mp3', id: 'A', keyCode: 97},
+    {src: './sounds/2.mp3', id: 'S', keyCode: 115},
+    {src: './sounds/3.mp3', id: 'D', keyCode: 100},
+    {src: './sounds/4.mp3', id: 'F', keyCode: 102},
+    {src: './sounds/5.mp3', id: 'G', keyCode: 103},
+    {src: './sounds/6.mp3', id: 'H', keyCode: 104},
+    {src: './sounds/7.mp3', id: 'J', keyCode: 106},
+    {src: './sounds/8.mp3', id: 'K', keyCode: 107},
+]
 
-function AudioPlayer() {
-    let audio = new Audio();
-    const Aplay = document.getElementById("A"); console.log(Aplay);
-    Aplay.addEventListener("click", divplay("playA"));
-    const Splay = document.getElementById("S");
-    Splay.addEventListener("click", divplay("playS"));
+const audio = new Audio();
 
-    function divplay(note) {
-        switch (note) {
-            case "playA":               
-              audio.src = "sounds/1.mp3"; console.log(audio.src);
-              break;
-            case "playS":
-              audio.src = "sounds/2.mp3";
-        }  
-        audio.play();                             
-    }
+function playNote(value){    
+        audio.src = value.src;
+        audio.play(); 
+};
+
+function audioPlayer() {    
+    notes.forEach(value => {
+        document.getElementById(value.id).addEventListener("click",  () => playNote(value))
+    })
 }
-window.addEventListener("load", AudioPlayer);
 
-//const divs = document.getElementsByClassName('pianoKey');
-//console.log(divs);
+function audioPlayerByKey(event) {
+    playNote(notes.filter(item => item.keyCode === event.keyCode)[0])
+};
 
-/*
-[...divs].forEach(element => {
-    element.addEventListener('click', function() { 
-        document.getElementsById('audio1').play();}
-    );
+window.addEventListener("load", audioPlayer);
 
-})
-*/
+window.addEventListener("keypress", audioPlayerByKey);
